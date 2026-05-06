75a16885b8f0684a78b29f08780aa164eac1c3aa
Harley D. Eades III <harley.eades@pm.me>
2026-05-06 15:26:02 -0400

    Working templates.

--- src/Data/Template.hs
+++ src/Data/Template.hs
@@ -6,8 +6,8 @@
 {-# LANGUAGE StandaloneKindSignatures #-}
 {-# LANGUAGE TypeFamilies #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# OPTIONS_GHC -Wno-unused-top-binds #-}
 {-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE TypeApplications #-}
 {-|
 Module      : Template
 Description : Framework for creating string templates
@@ -17,78 +17,71 @@ Maintainer  : harley.eades@wkb3.com
 
 Framework for creating string templates
 -}
-module Data.Template () where
--- External Imports:
+module Data.Template ((<+>)
+                     ,hole
+                     ,chunk
+                     ,plugHoles
+                     ,unpack) where
 
-import Prelude hiding (const)
-import Data.Text qualified as DT
-import Data.Kind (Constraint, Type)
-import Data.Coerce (coerce)
-import GHC.TypeNats (SNat, type (+), KnownNat, type (<=), Natural)
+import GHC.TypeNats (type (+)
+                    ,Natural)
 import GHC.TypeLits (Nat)
-import Data.Sized (Sized)
-import Data.Vector (Vector)
-import Data.Type.Ordinal (Ordinal, sNatToOrd, naturalToOrd, inclusion)
-import Data.Type.Natural (S)
 
--- Internal Imports:
+import Data.Text qualified as DT
 
-{- Example quasi-quoted string into datatype:
-{"f1":<1>,"f2":{"f21": <2>,"f22":"foo bar"}}
-=> '{"f1":' <> <1> <> ',"f2":{"f21":' <> <2> <> ',"f22":"foo bar"}}'
-=> '{"f1":' <> (Hole "1") <> ',"f2":{"f21":' <> (Hole 2) <> ',"f22":"foo bar"}}'
-=> (Chunk "{\"f1\":\"") <> (Hole "1") <> (Chunk ",\"f2\":{\"f21\":") <> (Hole "2") <> (Chunk ",\"f22\":\"foo bar\"}}")
--}
+data Template (n :: Nat) where
+  Hole    :: Natural -> Template 1
+  Chunk   :: DT.Text -> Template 0 
+  Compose :: (Template n1) -> (Template n2) -> Template (n1 + n2)  
 
-data ITemplate (n :: Nat) where
-  Hole    :: Natural -> ITemplate 1
-  Chunk   :: DT.Text -> ITemplate 0 
-  Compose :: (ITemplate n1) -> (ITemplate n2) -> ITemplate (n1 + n2)
-
-instance Show (ITemplate n) where
-    show :: ITemplate n -> String
+instance Show (Template n) where
+    show :: Template n -> String
     show (Hole i)        = "Hole" <> (show i)
     show (Chunk t)       = "Chunk" <> (show t)
     show (Compose t1 t2) = "Compose (" <> show t1 <> ") (" <> show t2 <> ")"
 
-(<+>) :: (ITemplate n1) -> (ITemplate n2) -> ITemplate (n1 + n2)
-(<+>) = Compose
+(<+>) :: Template n1
+      -> Template n2
+      -> Template (n1 + n2)
+t1 <+> t2 = Compose t1 t2
 
-hole :: Natural
-     -> ITemplate 1
+hole :: Natural -> Template 1
 hole i = Hole i
 
-chunk :: DT.Text
-      -> ITemplate 0
+chunk :: DT.Text -> Template 0
 chunk = Chunk
 
-_plugHoles 
-    :: (Natural -> DT.Text)
-    -> ITemplate n
-    -> ITemplate 0
-_plugHoles f (Hole i) = Chunk $ f i
-_plugHoles f (Compose t1 t2)  = Compose (_plugHoles f t1) (_plugHoles f t2)
-_plugHoles _ (Chunk t)        = Chunk t
+plugHoles 
+    :: (Natural -> Maybe DT.Text)
+    -> Template n
+    -> Maybe (Template 0)
+plugHoles f (Hole i) = do
+    c <- f i 
+    return $ Chunk c
+plugHoles f (Compose t1 t2) = do
+    t1' <- plugHoles f t1
+    t2' <- plugHoles f t2
+    return $ Compose t1' t2'
+plugHoles _ (Chunk t) = return $ Chunk t
 
 unpack 
-    :: ITemplate 0
+    :: Template 0
     -> DT.Text
 unpack = _unpack
     where
-        _unpack :: ITemplate n 
-               -> DT.Text
+        _unpack :: Template n -> DT.Text
         _unpack (Chunk s)       = s        
         _unpack (Compose t1 t2) = _unpack t1 <> _unpack t2
         _unpack (Hole _)        = error "reached unreachable branch"
 
--- exampleTemp :: ITemplate 3
--- exampleTemp = ((chunk "{\"f1\":\"") 
---         <+> (hole 1) )
---         <+> (chunk (",\"f2\":{\"f21\":")) 
---         <+> (hole 2) 
---         <+> (chunk ",\"f22\":\"foo bar\"}}")
+_exampleTemp :: Template 2
+_exampleTemp = (chunk "{\"f1\":\"") 
+        <+> (hole 1)
+        <+> (chunk (",\"f2\":{\"f21\":")) 
+        <+> (hole 2) 
+        <+> (chunk ",\"f22\":\"foo bar\"}}")
 
--- plug1 :: Int -> DT.Text
--- plug1 1 = "Staci"
--- plug1 2 = "Jo"
--- plug1 _ = ""
+_plug1 :: Natural -> Maybe DT.Text
+_plug1 1 = Just "Staci"
+_plug1 2 = Just "Jo"
+_plug1 _ = Nothing
